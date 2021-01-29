666c9ec35576daece7274fbea308b7c67e01f1a4
SageTengu <kyle.kleven@outlook.com>
2021-01-28 19:47:19 -0600

    Updated with latest js

--- script/script.js
+++ script/script.js
@@ -1,29 +1,35 @@
-var corsVar = "https://chriscastle.com/proxy/index.php?:proxy:"    
-var desZipcode = "90742"
+var corsVar = "https://chriscastle.com/proxy/index.php?:proxy:";
+var desZipcode = "90742";
 
-var zipcode = $('#input').val();
+var zipcode;
+var keyword;
+
+var npsAPIkey = "3eMx7JuhaDduCgDGcbpUQDSwo9EBymREAUXmdQch";
+var npsQueryURL = "https://developer.nps.gov/api/v1/activities?q=" + keyword + "&api_key=" + npsAPIkey;
 
 var weatherapiKey = "73d3cee72322c512646546f162d5afe5";
 var weatherqueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "&units=imperial&appid=" + weatherapiKey;
 
-var activityAPIKey = "hx8d23rqt44s3d3852d96x4v";
-var activityqueryURL = corsVar + "http://api.amp.active.com/v2/search/?near=" + zipcode + "&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=" + activityAPIKey;    
-    
+// NOT IN USE
+// var activityAPIKey = "hx8d23rqt44s3d3852d96x4v";
+// var activityqueryURL = corsVar + "http://api.amp.active.com/v2/search/?near=" + zipcode + "&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=" + activityAPIKey;    
+
 
 var zipcodeAPIKey = "ZjHz81gGy2vO2MQx3iYvDmmBBljAfRQzjbTg85zxDDPqSDZGjFiygyYRPNenp2pR";
 var zipCodequeryURL = corsVar + "https://www.zipcodeapi.com/rest/" + zipcodeAPIKey + "/distance.json/" + zipcode + "/" + desZipcode + "/mile";
 
-
-$('#submit').on("click",function(e){
+$('#submit').on("click", function(e) {
     e.preventDefault();
-    zipcode = $('#input').val();
-    weatherqueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "&units=imperial&appid=" + weatherapiKey;
-    console.log("Zip Code: " + zipcode);
-    
+    alert("this worked");
+    zipcode = document.querySelector('#input').value();
+    console.log(zipcode);
+    activity = $('option').text();
 
-    $.ajax({    
-    url: weatherqueryURL,
-    method: "GET"
+    weatherqueryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "&units=imperial&appid=" + weatherapiKey;
+
+    $.ajax({
+        url: weatherqueryURL,
+        method: "GET"
     }).then(function (weather) {
         console.log(weatherqueryURL);
         console.log(weather.main.temp);
@@ -32,37 +38,38 @@ $('#submit').on("click",function(e){
         console.log(weather.main.humidity);
         console.log(weather.name);
         console.log(moment((weather.dt), "X").format("MM/DD/YY"));
-        //compareZip();
+        compareZip();
         getActivity();
-        
+
     });
 
-    function getActivity() {
-    activityqueryURL = corsVar + "http://api.amp.active.com/v2/search/?near=" + zipcode + "&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=" + activityAPIKey;    
-    
-        $.ajax({
+
+});
+
+function getActivity() {
+    activityqueryURL = corsVar + "http://api.amp.active.com/v2/search/?near=" + zipcode + "&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=" + activityAPIKey;
+
+    $.ajax({
         url: activityqueryURL,
         method: "GET",
         // headers: {'Origin': "http://api.amp.active.com/v2/search/?zip=" + zipcode + "&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=" + activityAPIKey}
-        }).then(function (activities) {
-            console.log(JSON.parse(activities));
-            
-            
-            for (var i = 0; i < 5; i++) {
-                console.log(JSON.parse(JSON.stringify(activities[i].assetName))); 
-            }
-                                    
-        });
-    }
+    }).then(function (activities) {
+        console.log(JSON.parse(activities));
 
 
-    function compareZip() {
-    
-        $.ajax({
+        for (var i = 0; i < 5; i++) {
+            console.log(JSON.parse(JSON.stringify(activities[i].assetName)));
+        }
+
+    });
+}
+
+function compareZip() {
+
+    $.ajax({
         url: zipCodequeryURL,
         method: "GET"
-        }).then(function (zipCompare) {
-            console.log(zipCodequeryURL);
-        });
-    }
-});
+    }).then(function (zipCompare) {
+        console.log(zipCodequeryURL);
+    });
+}
