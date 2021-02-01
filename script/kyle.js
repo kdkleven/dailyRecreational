e07a28f82305e5f92a4df9c2273e7f3e7ca5f50f
Kyle Kleven <kylekleven8@gmail.com>
2021-02-01 17:07:07 -0600

    Fixed an error wth missing curlybraces, and image spacing on cards

--- script/kyle.js
+++ script/kyle.js
@@ -76,7 +76,7 @@ function getParkInfo(stateInput, activityInput) {
    
             //parkDiv.css({ });
             parkName.html(result[a].fullName);
-            img.attr({src: result[a].images[0].url, class: });
+            img.attr({src: result[a].images[0].url, class: 'parkImg'});
             parkDescription.html(result[a].description);
             parkURL.html(result[a].url);
             // parkLat.html("Latitude: " + result[a].latitude);
